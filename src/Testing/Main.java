8407c3137decc3ad36f71e0725d819a122e3bddd
VaibhavChavan1998 <vaibhavrc1@gmail.com>
2025-03-16 21:21:41 +0530

    Removed unused imports

--- src/Testing/Main.java
+++ src/Testing/Main.java
@@ -1,8 +1,5 @@
 package Testing;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
